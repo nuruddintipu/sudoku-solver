6f29f405369f17a569c432a1f7a0e5f425c9937e
nuruddintipu <nuruddintipu.connect@gmail.com>
2024-12-09 04:16:14 +0600

    refactored code

--- src/components/board/ControlBoard.js
+++ src/components/board/ControlBoard.js
@@ -1,17 +1,33 @@
 import React from "react";
-import Cell from "./Cell";
 
 function ControlBoard( { onClick }) {
     const numbers = [1,2,3,4,5,6,7,8,9];
+
+    const defaultStyle = {
+        cell: {
+            width: '40px',
+            height: '40px',
+            fontSize: '20px',
+            textAlign: 'center',
+            lineHeight: '34px',
+            fontWeight: 'bold',
+            border: '1px solid #999',
+            boxSizing: 'border-box',
+            cursor: 'pointer',
+        }
+    };
+
     return (
         <div className="control-board" style={{display: 'flex' , marginTop: '20px'}}>
             {numbers.map((num) => (
-                <Cell
+                <div
                     key={num}
                     value={num}
                     onClick ={() => onClick(num)}
-                    style={{cursor: 'pointer', background: '#eee'}}
-                />
+                    style={{...defaultStyle.cell}}
+                >
+                    {num}
+                </div>
             ))}
         </div>
     );
